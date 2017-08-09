ee944e6fe095d054441dd9f658ca9c10878216cf
carmen <carmen-walser@gmx.ch>
2017-08-09 21:38:11 +0200

    Auskommentierte Zeilen gelöscht

--- src/kamisado/client/anmeldefenster/AnmeldefensterController.java
+++ src/kamisado/client/anmeldefenster/AnmeldefensterController.java
@@ -114,59 +114,7 @@ public class AnmeldefensterController {
 						InfofensterController icontroller = new InfofensterController(iview);
 						iview.start();
 						stage.setAlwaysOnTop(true);
-				 }
-
-				 
-				 
-				 			 				 
-//				 //überprüfen, ob Benutzername bereits vergeben ist
-//				 try {
-//					FileReader fr = new FileReader("registrierungen.txt");
-//					BufferedReader reader = new BufferedReader(fr);
-//					String zeile;
-//					Label label;
-//					boolean benutzerVergeben = false;
-//					while((zeile=reader.readLine())!= null){
-//						String[] parts = zeile.split(",");
-//							
-//						if(parts[0].equals(name)){
-//							//Meldung, dasss Benutzername bereits vergeben ist
-//							Stage stage = new Stage();
-//							InfofensterView iview = new InfofensterView(stage, anmeldeView.BenutzernameVergeben);
-//							iview.start();
-//							stage.setAlwaysOnTop(true);
-//							benutzerVergeben = true;
-//						} 
-//					}
-//					//Wenn der Benutzername nicht vergeben ist und das Passwort genügend Zeichen beinhaltet, werden die Daten in das File geschrieben
-//					if(benutzerVergeben==false){
-//						if(pw.length()>=5){
-//							FileWriter fw = new FileWriter("src/kamisado/registrierungen.txt", true);
-//							fw.write(name+",");
-//							fw.write(pw);
-//							fw.write("\n");
-//							fw.close();
-//							
-//							//Meldung, dass die Registrierung erfolgreich war
-//							Stage stage = new Stage();
-//							InfofensterView iview = new InfofensterView(stage,anmeldeView.RegistrierMeldung);
-//							InfofensterController icontroller = new InfofensterController(iview);
-//							iview.start();
-//							stage.setAlwaysOnTop(true);
-//						}	else{
-//							//Meldung, dass Passwort zu kurz ist
-//							Stage stage = new Stage();
-//							InfofensterView iview = new InfofensterView(stage,anmeldeView.PasswortZuKurz);
-//							InfofensterController icontroller = new InfofensterController(iview);
-//							iview.start();
-//							stage.setAlwaysOnTop(true);
-//						}
-//					}
-//					
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//				}
-				 
+				 }				 
 			}
 		});
 		
